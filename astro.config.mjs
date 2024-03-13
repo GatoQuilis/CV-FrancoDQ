7837e2ad44b9ffe1545a19e9533d438163615586
GatoQuilis <franko_222@hotmail.com>
2024-03-12 23:47:19 -0300

    Agregando nuevo puesto

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://GatoQuilis.github.com',
+    base: '/CV-FrancoDQ',
+});
